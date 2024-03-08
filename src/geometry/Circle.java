71e5f9f4198506e0122581c92b45110e09ec4592
DipakRGadekar <Dipugadekar2013@gmail.com>
2024-03-09 00:17:01 +0530

    Geometry program

---
+++ src/geometry/Circle.java
@@ -0,0 +1,11 @@
+package geometry;
+
+public class Circle {
+
+	public double radius;
+
+	public Circle(double radius) 
+	{
+		this.radius= radius;
+	}
+}
---
+++ src/geometry/Rectangle.java
@@ -0,0 +1,12 @@
+package geometry;
+
+public class Rectangle {
+	public int length;
+	public int width;
+
+	public Rectangle(int length, int width) 
+	{
+		this.length= length;
+		this.width = width;
+	}
+}
