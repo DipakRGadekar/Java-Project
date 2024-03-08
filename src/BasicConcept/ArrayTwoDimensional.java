7f3e8ef0a4879e86256a815f65ece0124ed6db71
DipakRGadekar <Dipugadekar2013@gmail.com>
2024-03-09 00:19:43 +0530

    Two dimensional Array program

--- src/BasicConcept/ArrayTwoDimensional.java
+++ src/BasicConcept/ArrayTwoDimensional.java
@@ -7,6 +7,8 @@ public class ArrayTwoDimensional {
 		
 		int [][] arr = {{10,40,50},{70,80,30}};
 	
+		System.out.println("Print the number present in row 0 and column 2: " + arr[0][2]);
+		
 		for (int i =0; i<=1; i++) 
 		{
 			for(int j =0; j<=2; j++)  
